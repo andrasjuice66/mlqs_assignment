ae883d3cd44d4f0d2cea581899f600fb6b7f03b7
josh <joshwbrook01@gmail.com>
2024-06-14 11:23:51 +0200

    update gitignore and feature aggregation

--- 02_feature_engineering.py
+++ 02_feature_engineering.py
@@ -7,26 +7,33 @@ from src.FrequencyAbstraction import FourierTransformation
 import time
 
 
+def calc_aggs(df, cols, window, method="mean"):
+    num_abs = NumericalAbstraction()
 
-def aggregation_features(df):
+    for c in cols:
+        df[c+""+method] = num_abs.aggregate_value(df[c], window, method)
+    return df
+
+
+def aggregation_features(df, window_size=10):
 
     # Initialize the NumericalAbstraction class
-    num_abs = NumericalAbstraction()
 
     # List of columns to apply the aggregation functions
     cols = ["Acceleration x (m/s^2)", "Acceleration y (m/s^2)", "Acceleration z (m/s^2)",
-                        "Gyroscope x (rad/s)", "Gyroscope y (rad/s)", "Gyroscope z (rad/s)",
-                        "Velocity (m/s)", "Height (m)", "Linear Acceleration x (m/s^2)", 
-                        "Linear Acceleration y (m/s^2)", "Linear Acceleration z (m/s^2)"]
-    window_size = 10
+            "Gyroscope x (rad/s)", "Gyroscope y (rad/s)", "Gyroscope z (rad/s)",
+            "Linear Acceleration x (m/s^2)", "Linear Acceleration y (m/s^2)", "Linear Acceleration z (m/s^2)",
+            "Velocity (m/s)", "Height (m)"]
 
-    df[cols] = df[cols].abs()
+    # df[cols] = df[cols].abs()
 
     # Apply various aggregations
     aggregations = ['mean', 'max', 'min', 'std', 'slope']
     for agg in aggregations:
-        df = num_abs.abstract_numerical(df, cols, window_size, agg)
+        print("Applying method", agg)
+        df = calc_aggs(df, cols, window_size, method=agg)
     return df
+
 
 def frequency_features(df):
     # Convert the 'Datetime_linacc' column to datetime and set as index
@@ -44,6 +51,7 @@ def frequency_features(df):
     df_transformed = ft.abstract_frequency(df, columns, window_size, sampling_rate)
     return df_transformed
 
+
 def main():
     print("Feature enigneering starts...")
     start_time = time.time()
@@ -51,7 +59,7 @@ def main():
 
     df = aggregation_features(df)
     end_time_agg = time.time()
-    print(f"Aggregation feature engineering ended in  {end_time_agg - start_time:.2f} seconds.")
+    print(f"Aggregation feature engineering ended in {end_time_agg - start_time:.2f} seconds.")
 
     df = frequency_features(df)
     end_time_freq = time.time()
@@ -59,7 +67,7 @@ def main():
 
     df.to_csv(f"data_agg/feature_engineered.csv", index=False)
     end_time = time.time()
-    print(f"Exported feature_engineered.csv in  {end_time_freq - end_time:.2f} seconds.")
+    print(f"Exported feature_engineered.csv in {end_time_freq - end_time:.2f} seconds.")
     print(f"Feature engineering endid in total of {end_time - start_time:.2f} seconds.")
 
 if __name__ == '__main__':
